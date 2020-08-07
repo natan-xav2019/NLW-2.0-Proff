16545cf77987e8b547912c85c6d4042e59c0e0ad
Natan Xavier <natanxaviersiquera@gmail.com>
2020-08-06 21:12:05 -0300

    aula do video das 05/08

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,54 @@ import React from 'react';
 import whatsappLogo from "../../assets/images/icons/whatsapp.svg";
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem () {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+    
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Diego Fernandes"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                     <div>
-                        <strong>Diego Fernandes</strong>
-                        <span>Quimica</span>
+                        <strong>{teacher.name}</strong>
+                        <span>{teacher.subject}</span>
                     </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de quimica avançada.
-                <br/><br/>
-                Apaixonado por explodir coisas em laboratårio e por mudar a Vida das pessoas através de experiéncias. Mais de 200.000 pessoas jå passaram por uma das minhas explosöes.
-            </p>
+            <p> {teacher.bio} </p>
 
             <footer>
                 <p>
                     preço/hora
-                    <strong>R$20,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a
+                    target="_blank"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappLogo} alt="Whatsapp"/>
                         Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -8,9 +8,21 @@ import stundyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import api from '../../services/api'
+
 import './styles.css';
 
 function Landing() {
+    const [totalConennections, SetTotalConnections] = useState(0);
+
+    useEffect(()=> {
+        api.get('connections').then(response => {
+            const { total } = response.data
+
+            SetTotalConnections(total);
+        })
+    }, []);
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -39,7 +51,7 @@ function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões ja realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                    Total de {totalConennections} conexões ja realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span>
             </div>
         </div>
